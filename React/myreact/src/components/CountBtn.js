c76c632dfa69304d9705f2e895c2d79e2b802a71
AyushJaiswal300404 <ayushjaiswal300404@gmail.com>
2025-03-13 01:25:01 +0530

    Day-30

---
+++ React/myreact/src/components/CountBtn.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CountBtn = () => {
+    return <div>Hello World</div>
+}
+
+export default CountBtn
---
+++ React/myreact/src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CountBtn from './CountBtn';
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Counter App</h1>
+            <CountBtn />
+        </div>
+    )
+};
+
+export default App
