b845112af230db873abce44be96f25a8e281d0d5
AyushJaiswal300404 <ayushjaiswal300404@gmail.com>
2025-04-21 02:45:00 +0530

    Day-57

--- WanderLust/routes/listing.js
+++ WanderLust/routes/listing.js
@@ -2,29 +2,21 @@ const express = require('express');
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, validateListing, isOwner } = require("../middleware.js");
-
 const listingController = require("../controllers/listing.js");
 
-//Index Route
-router.get("/",  wrapAsync(listingController.index));
+router
+    .route("/")
+    .get(wrapAsync(listingController.index))                                                              //Index Route
+    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));                       //Create Route
 
-//New Route
-router.get("/new",isLoggedIn, listingController.renderNewForm);
+router.get("/new",isLoggedIn, listingController.renderNewForm);                                           //New Route
 
-//Show Route
-router.get("/:id", wrapAsync(listingController.showListings));
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListings))                                                //Show Route
+    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))        //Update Route
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));                    //Delete Route
 
-//Create Route
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing));
-
-//Edit Route
-router.get("/:id/edit", isLoggedIn,isOwner, wrapAsync(listingController.renderEditForm));
-
-//Update Route
-router.put("/:id", isLoggedIn,isOwner, validateListing,wrapAsync(listingController.updateListing));
-
-
-//Delete Route
-router.delete("/:id", isLoggedIn,isOwner, wrapAsync(listingController.destroyListing));
+router.get("/:id/edit", isLoggedIn,isOwner, wrapAsync(listingController.renderEditForm));                  //Edit Route
 
 module.exports = router;
