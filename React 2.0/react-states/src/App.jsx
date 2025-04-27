afac2b6f3c4b41959b9e1890229a3a691b1199f9
AyushJaiswal300404 <ayushjaiswal300404@gmail.com>
2025-04-28 00:48:06 +0530

    Day-69

--- React 2.0/react-states/src/App.jsx
+++ React 2.0/react-states/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
-import TodoList from './TodoList'
+import Lottery from './Lottery'
+import TicketNum from './TicketNum'
+import Ticket from './Ticket'
 
 function App() { 
   return (
     <>
-      <TodoList />
+      <h1>Lottery Game</h1>
+      <Lottery n={3} winningSum={10}/><h1>Lottery Game</h1>
+      <Lottery n={5} winningSum={98}/>
     </>
   )
 }
---
+++ React 2.0/react-states/src/Lottery.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import TicketNum from './TicketNum';
+
+function Lottery({ n, winningSum }) {
+    const [ticket, setTicket] = useState(new Array(n).fill(0));
+    
+    const generateTicket = () => {
+        setTicket(ticket.map(() => Math.floor(Math.random() * 10)));
+    }
+
+    const sum = ticket.reduce((sum, curr) => sum + curr, 0);
+    const isWinning = sum === winningSum;
+
+    return (
+        <div>
+            <div className="ticket">
+                {ticket.map((num, idx) => (
+                    <TicketNum key={idx} num={num} />
+                ))}
+            </div>
+            <button onClick={generateTicket}>Generate</button>
+            {isWinning && <h2>Congratulations! You Won!</h2>}
+        </div>
+    );
+}
+
+export default Lottery;
